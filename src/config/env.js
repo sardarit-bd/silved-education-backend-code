c2cf7e4aff96a4fe01bc3d8f3580a73b3ea8d8ad
Md Emon Hossen <mdemong87@gmail.com>
2025-11-07 17:18:27 +0600

    fixed isses

--- src/config/env.js
+++ src/config/env.js
@@ -1,8 +1,11 @@
 import dotenv from "dotenv";
 import path from "path";
+import { createRequire } from 'module';
 
+const require = createRequire(import.meta.url);
 const environment = process.env.NODE_ENV || "development";
 const envPath = path.join(process.cwd(), `.env.${environment}`);
 dotenv.config({ path: envPath });
 
 export default environment;
+
